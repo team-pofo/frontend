aa85971acaa6d21ae15621d8d48a8a903f36b165
jihunchoi <geehoon62017@gmail.com>
2024-10-06 18:21:38 +0900

    emotion 설정 및 redux 설치

---
+++ src/pages/about.tsx
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+
+export const TestEmotion = styled.h1`
+  font-size: 2rem;
+  color: aqua;
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import localFont from "next/font/local";
 import styles from "@/styles/Home.module.css";
+import { TestEmotion } from "@/styles/aboutStyle";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -42,6 +43,7 @@ export default function Home() {
             <li>Save and see your changes instantly.</li>
           </ol>
 
+          <TestEmotion>TEST emotion css</TestEmotion>
           <div className={styles.ctas}>
             <a
               className={styles.primary}
