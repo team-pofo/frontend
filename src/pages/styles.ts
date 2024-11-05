cabb5985c00b99333815ef9524e8062b5fc70cf1
jihunchoi <geehoon62017@gmail.com>
2024-11-05 17:16:40 +0900

    style. #7-프로젝트 그리드 반응형 설정

--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -1,23 +1,29 @@
-// styles/styles.ts
 import styled from "@emotion/styled";
 
 export const GridContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
   gap: 16px;
   padding: 20px;
   max-width: 1800px;
 
-  @media (min-width: 1200px) {
+  // 1920px 이상일 때 5열
+  @media (min-width: 1920px) {
     grid-template-columns: repeat(5, 1fr);
   }
-  @media (max-width: 1200px) and (min-width: 768px) {
-    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  // 1200px 이상 1920px 미만일 때 4열
+  @media (min-width: 1200px) and (max-width: 1919px) {
+    grid-template-columns: repeat(4, 1fr);
   }
-  @media (max-width: 768px) {
-    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  // 768px 이상 1200px 미만일 때 3열
+  @media (min-width: 768px) and (max-width: 1299px) {
+    grid-template-columns: repeat(3, 1fr);
   }
-  @media (max-width: 480px) {
+  // 480px 이상 768px 미만일 때 2열
+  @media (min-width: 480px) and (max-width: 999px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  // 480px 미만일 때 1열
+  @media (max-width: 699px) {
     grid-template-columns: 1fr;
   }
 `;
