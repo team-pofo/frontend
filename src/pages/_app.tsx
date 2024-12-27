77d107e62bdfa5d1f23afec64b17369824c2d913
kevinmj12 <kevinmj12@gmail.com>
2024-12-27 11:10:16 +0900

    feat/#19/libs 통일

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,9 +11,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={myFont.className}>
       <Layout>
-          <ApolloProvider client={client}>
-            <Component {...pageProps} />
-          </ApolloProvider>
+        <ApolloProvider client={client}>
+          <Component {...pageProps} />
+        </ApolloProvider>
       </Layout>
     </div>
   );
