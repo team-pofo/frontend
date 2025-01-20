1a4ea2053e5ba7970ee26c73d0eaa2136673401e
Jihun Choi <geehoon62017@gmail.com>
2025-01-20 13:35:33 +0900

    fix. #57-Image warning 해결

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -37,8 +37,8 @@ const ProjectCard = forwardRef<HTMLDivElement, IProjectCardProps>(
                   : "https://velog.velcdn.com/images/yena1025/post/295eb434-5b73-421f-bbe4-6bc13acd4c33/image.png"
               }
               alt={title}
-              layout="fill"
-              objectFit="cover"
+              fill
+              style={{ objectFit: "cover" }}
             />
           </S.ImageWrapper>
           <S.Content>
